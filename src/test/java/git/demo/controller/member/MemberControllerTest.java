faf2db8782d51b64913a432d3c279dfdcb250a3a
LeeYoungJin <dudwls0505@gmail.com>
2022-01-09 17:31:23 +0900

    refactor: MemberController의 join성공,실패경우의 테스트코드추가 + 변경

--- src/test/java/git/demo/controller/member/MemberControllerTest.java
+++ src/test/java/git/demo/controller/member/MemberControllerTest.java
@@ -7,7 +7,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest
 class MemberControllerTest {
@@ -19,14 +19,30 @@ class MemberControllerTest {
     void joinGetTest() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/member/join"))
                 .andDo(print())
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(view().name("member/join"))
+                .andExpect(model().attributeExists("member"));
     }
 
     @Test
-    void joinPosTest() throws Exception {
+    void joinPostTest() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/member/join")
+                .param("userId" , "YoungJin")
+                .param("userPw", "juliet12"))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/"));
+    }
+
+    @Test
+    void joinPostHasErrors() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.post("/member/join"))
                 .andDo(print())
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(model().attributeHasErrors("member"))
+                .andExpect(model().attributeHasFieldErrors("member", "userId"))
+                .andExpect(model().attributeHasFieldErrors("member", "userPw"))
+                .andExpect(view().name("member/join"));
     }
 
 }
