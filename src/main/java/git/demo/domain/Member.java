7c857ed066fa1e5f163b78e2903f4de751a33a54
LeeYoungJin <dudwls0505@gmail.com>
2022-01-09 17:24:24 +0900

    refactor: Member 필드검증어노테이션 @NotNull -> @NotEmpty로변경, 검증메시지추가

--- src/main/java/git/demo/domain/Member.java
+++ src/main/java/git/demo/domain/Member.java
@@ -1,11 +1,14 @@
 package git.demo.domain;
 
-import javax.validation.constraints.NotNull;
 
+import lombok.Data;
+import javax.validation.constraints.NotEmpty;
+
+@Data
 public class Member {
 
-    @NotNull
+    @NotEmpty
     private String userId;
-    @NotNull
+    @NotEmpty(message = "비밀번호를 입력해주세요")
     private String userPw;
 }
