347ed8c6e70a724cba02702238d3f8726809f5ca
youngreal <dudwls0505@naver.com>
2022-01-10 19:42:04 +0900

    feat: MemberRepository에  findAll() findByLoginId 메소드 추가

--- src/main/java/git/demo/repository/member/MemberRepository.java
+++ src/main/java/git/demo/repository/member/MemberRepository.java
@@ -5,7 +5,10 @@ import git.demo.domain.Member;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Slf4j
@@ -22,5 +25,16 @@ public class MemberRepository {
         return member;
     }
 
+
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+
+    public Optional<Member> findByLoginId(String loginId) {
+        return findAll().stream()
+                .filter(m-> m.getUserPw().equals(loginId))
+                .findFirst();
+    }
 
 }
