ed96143b6549e857655b4c434e2439832f880b71
youngreal <dudwls0505@naver.com>
2022-01-12 22:43:52 +0900

    feat: LoginController 로그아웃 세션종료 추가, 로그인한유저 세션추가

--- src/main/java/git/demo/controller/member/LoginController.java
+++ src/main/java/git/demo/controller/member/LoginController.java
@@ -4,6 +4,7 @@ import git.demo.domain.Member;
 import git.demo.domain.login.LoginForm;
 import git.demo.domain.login.LoginService;
 import git.demo.repository.member.MemberRepository;
+import git.demo.session.SessionConst;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -12,7 +13,10 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
 @Controller
@@ -23,15 +27,27 @@ public class LoginController {
     private final LoginService loginService;
     private final MemberRepository memberRepository;
 
+    @GetMapping("/")
+    public String homeLogin(@SessionAttribute(value = SessionConst.LOGIN_MEMBER, required = false) Member loginMember, Model model) {
+
+        if (loginMember == null) {
+            System.out.println("loginMember==null");
+            return "index";
+        }
+        model.addAttribute("member", loginMember);
+        return "member/loginON";
+    }
+
+
     @GetMapping("member/login")
-    public String loginMemberForm(Model model){
+    public String loginMemberForm(Model model) {
         model.addAttribute("loginForm", new LoginForm());
         log.info("GetMapping login 실행");
         return "member/login";
     }
 
     @PostMapping("member/login")
-    public String loginMember(@Valid @ModelAttribute LoginForm loginForm, BindingResult bindingResult) {
+    public String loginMember(@Valid @ModelAttribute LoginForm loginForm, BindingResult bindingResult, HttpServletRequest request) {
 
         if (bindingResult.hasErrors()) {
             log.info("errors={}", bindingResult);
@@ -45,8 +61,20 @@ public class LoginController {
             bindingResult.reject("loginFail", "아이디 혹은 비밀번호가 맞지 않습니다.");
             return "member/login";
         }
-        
+
+        HttpSession session = request.getSession();
+        session.setAttribute(SessionConst.LOGIN_MEMBER, loginMember);
+
+
         return "redirect:/";
     }
 
+    @PostMapping("/logout")
+    public String logoutMember(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            session.invalidate();
+        }
+        return "redirect:/";
+    }
 }
