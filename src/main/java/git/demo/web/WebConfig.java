1f1bb8de1872352c4a0daf564043aab1ab955818
youngreal <dudwls0505@naver.com>
2022-01-15 01:45:17 +0900

    refactor: WebConfig에 UrlInteceptor 추가

--- src/main/java/git/demo/web/WebConfig.java
+++ src/main/java/git/demo/web/WebConfig.java
@@ -1,6 +1,7 @@
 package git.demo.web;
 
 import git.demo.web.interceptor.FileInterceptor;
+import git.demo.web.interceptor.UrlInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -11,11 +12,16 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new FileInterceptor())
+
+        registry.addInterceptor(new UrlInterceptor())
                 .order(1)
                 .addPathPatterns("/**")
-                .excludePathPatterns("/","/member/join","/member/login","/logout","/css/**", "/error");
+                .excludePathPatterns("/","/member/join","/member/login","/logout","/css/**", "/error", "/file/upload" , "/error-*");
+
+
+        registry.addInterceptor(new FileInterceptor())
+                .order(2)
+                .addPathPatterns("/file/upload");
+
     }
-
-
 }
