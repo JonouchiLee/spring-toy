e4d99d8c853e0f9ebfef76e2d0457ac4ab35c8c9
youngreal <dudwls0505@naver.com>
2022-01-20 00:36:26 +0900

    refactor: db연결후 login로직 변경

--- src/main/java/git/demo/domain/login/LoginService.java
+++ src/main/java/git/demo/domain/login/LoginService.java
@@ -1,20 +1,28 @@
 package git.demo.domain.login;
 
 import git.demo.domain.Member;
-import git.demo.repository.member.MemberRepository;
+import git.demo.mapper.MemberMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
 
 @Service
 @RequiredArgsConstructor
 public class LoginService {
 
-    private final MemberRepository memberRepository;
+    private final MemberMapper memberMapper;
+
+//    public Member login(String loginId, String loginPw) {
+//        return memberRepository.findByLoginId(loginId)
+//                .filter(m -> m.getUserPw().equals(loginPw))
+//                .orElse(null);
+//    }
 
     public Member login(String loginId, String loginPw) {
-        return memberRepository.findByLoginId(loginId)
-                .filter(m -> m.getUserPw().equals(loginPw))
-                .orElse(null);
+        Member memberLoginId = memberMapper.findLoginId(loginId, loginPw);
+        System.out.println("memberLoginId =" + memberLoginId);
+        return memberLoginId;
     }
+
 
 }
