e7165cd9427294e4ee67fd961972cf7d1f333ce1
youngreal <dudwls0505@naver.com>
2022-01-23 18:03:10 +0900

    feat: Book도메인추가 + 도메인경로정리

---
+++ src/main/java/git/demo/domain/book/Book.java
@@ -0,0 +1,28 @@
+package git.demo.domain.book;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+@Data
+public class Book {
+
+    private Long id;
+    @NotEmpty
+    private String bookName;
+    @NotNull(message = "가격을 입력해주세요")
+    private Integer price;
+    @NotNull(message = "수량을 입력해주세요")
+    private Integer quantity;
+
+    public Book() {
+    }
+
+    public Book(Long id, String bookName, Integer price, Integer quantity) {
+        this.id = id;
+        this.bookName = bookName;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
