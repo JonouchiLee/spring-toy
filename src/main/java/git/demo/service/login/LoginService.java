f042e955238d04f63c9afdfbe1e6d2497fae0cc8
youngreal <dudwls0505@naver.com>
2022-01-23 17:52:17 +0900

    refactor: LoginService import 정리

---
+++ src/main/java/git/demo/service/login/LoginService.java
@@ -0,0 +1,28 @@
+package git.demo.service.login;
+
+import git.demo.domain.member.Member;
+import git.demo.mapper.MemberMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+
+@Service
+@RequiredArgsConstructor
+public class LoginService {
+
+    private final MemberMapper memberMapper;
+
+//    public Member login(String loginId, String loginPw) {
+//        return memberRepository.findByLoginId(loginId)
+//                .filter(m -> m.getUserPw().equals(loginPw))
+//                .orElse(null);
+//    }
+
+    public Member login(String loginId, String loginPw) {
+        Member memberLoginId = memberMapper.findLoginId(loginId, loginPw);
+        System.out.println("memberLoginId =" + memberLoginId);
+        return memberLoginId;
+    }
+
+
+}
