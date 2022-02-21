729d23d0ec17542eb17e8ac7e5304598d703c7e9
youngreal <dudwls0505@naver.com>
2022-02-21 16:36:45 +0900

    feat: BookController 책목록전체읽기, 책상세정보읽기 Mapping추가

--- src/main/java/git/demo/controller/book/BookController.java
+++ src/main/java/git/demo/controller/book/BookController.java
@@ -2,17 +2,17 @@ package git.demo.controller.book;
 
 import git.demo.domain.book.Book;
 import git.demo.mapper.BookMapper;
-import git.demo.repository.book.BookRepository;
+import git.demo.service.book.BookService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Slf4j
 @Controller
@@ -20,7 +20,7 @@ import javax.validation.Valid;
 @RequestMapping("book")
 public class BookController {
 
-    private final BookRepository bookRepository;
+    private final BookService bookService;
     private final BookMapper bookMapper;
 
 
@@ -29,6 +29,7 @@ public class BookController {
         log.info("controlBookPage GetMapping 실행");
         return "book/controlBook";
     }
+
 
     @GetMapping("/createBook")
     public String insertBookForm(@ModelAttribute Book book) {
@@ -44,8 +45,47 @@ public class BookController {
             return "book/createBook";
         }
 
-        bookRepository.save(book);
+        bookService.save(book);
         return "redirect:/";
 
     }
+
+    @GetMapping("/readBookList")
+    public String readBookPage(Model model) {
+        log.info("readBookPage GetMapping 실행");
+        List<Book> books = bookMapper.findAllBook();
+        model.addAttribute("books", books);
+        System.out.println("books=" + books);
+        return "book/readBookList";
+    }
+
+
+    @GetMapping("/BookDetail/{bookId}")
+    public String readBookDetail(@PathVariable Long bookId, Model model) {
+        Book book = bookMapper.findBookById(bookId);
+        model.addAttribute("book", book);
+        log.info("readBookDetail getMapping 성공 그리고 book 값 = " + book);
+        return "book/BookDetail";
+    }
+
+    @GetMapping("/readBookList/{bookId}/edit")
+    public String editBook(@PathVariable Long bookId, Model model) {
+        Book book = bookMapper.findBookById(bookId);
+        model.addAttribute("book", book);
+        log.info("editBook getMapping 성공 그리고 book 값 = " + book);
+        return "book/updateBook";
+    }
+
+    @PostMapping("/readBookList/{bookId}/updateBookFin")
+    public String editBookPost(@PathVariable Long bookId, @Valid @ModelAttribute Book book, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+        // 값을 DB에 넣어야함
+        bookMapper.updateBook(book.getId(), book.getBookName(), book.getPrice(), book.getQuantity());
+        System.out.println("bookget정보들=" + book.getId()+book.getBookName()+book.getPrice()+book.getQuantity());
+        log.info("수정완료 ");
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/book/readBookList";
+    }
+
+
+
 }
