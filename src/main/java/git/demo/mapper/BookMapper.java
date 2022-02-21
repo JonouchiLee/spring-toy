e98d7b7cb739d9edb68c1f2531527db405872d39
youngreal <dudwls0505@naver.com>
2022-02-21 16:40:46 +0900

    feat: BookMapper findBookById, findAllBook, updtateBook 추가

--- src/main/java/git/demo/mapper/BookMapper.java
+++ src/main/java/git/demo/mapper/BookMapper.java
@@ -2,9 +2,18 @@ package git.demo.mapper;
 
 import git.demo.domain.book.Book;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 @Mapper
 public interface BookMapper {
 
     void insertBook(Book book);
+
+    Book findBookById(Long bookId);
+
+    List<Book> findAllBook();
+
+    void updateBook(@Param("id")Long id, @Param("bookName")String bookName, @Param("price")Integer price, @Param("quantity")Integer quantity);
 }
