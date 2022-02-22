c4b1bd3ed32d60793ccb85f3695a0e0d7efa3460
youngreal <dudwls0505@naver.com>
2022-02-23 02:11:56 +0900

    feat: 로그인상태에서 로그인페이지 접근불가 인터셉터추가

--- src/main/java/git/demo/web/WebConfig.java
+++ src/main/java/git/demo/web/WebConfig.java
@@ -1,6 +1,7 @@
 package git.demo.web;
 
 import git.demo.web.interceptor.FileInterceptor;
+import git.demo.web.interceptor.LoginOnIntercepter;
 import git.demo.web.interceptor.UrlInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -16,13 +17,20 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addInterceptor(new UrlInterceptor())
                 .order(1)
                 .addPathPatterns("/**")
-                .excludePathPatterns("/","/member/join","/member/login","/logout","/css/**",
-                        "/error", "/file/upload" , "/error-*", "/api/members/**", "/book/**");
+                .excludePathPatterns("/","/member/join","/member/login","/logout","/bootstrap/css/**","/bootstrap/assets/img/**",
+                        "/error", "/file/upload" , "/error-*", "/api/members/**", "/book/**"
+                        );
 
 
         registry.addInterceptor(new FileInterceptor())
                 .order(2)
                 .addPathPatterns("/file/upload" , "/book/**");
 
+
+        registry.addInterceptor(new LoginOnIntercepter())
+                .order(3)
+                .addPathPatterns("/member/login");
+
     }
+
 }
